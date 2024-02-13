f455548f57929b7ec79092c0a43406532edca165
sarpkocabagli <sarp.kocabagli@d-teknoloji.com.tr>
2024-02-13 11:25:10 +0300

    Initial Commit

---
+++ SwiftUILearn/SwiftUILearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearnApp.swift
+//  SwiftUILearn
+//
+//  Created by Sarp on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
