0e0c634b92707a546849dc57249d159c6cf338bc
sarpkocabagli <sarp.kocabagli@d-teknoloji.com.tr>
2024-02-13 16:08:05 +0300

    Creating Models

---
+++ SwiftUILearn/Model/ModelData.swift
@@ -0,0 +1,37 @@
+//
+//  ModelData.swift
+//  SwiftUILearn
+//
+//  Created by Sarp on 2/13/24.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("\(filename) doesn't exist")
+    }
+
+
+    do {
+        data = try Data(contentsOf: file)
+    } 
+    catch {
+        fatalError("\(filename) loading error:\n\(error)")
+    }
+
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    }
+    catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
